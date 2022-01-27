319839fdb24e29e36daa34a18e01028106d437fb
felixZmn <felix.zimmermann.1.de@gmail.com>
2022-01-27 16:17:36 +0100

    Step 3

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include "point.h"
+#include "stack/stack.h"
+#include <time.h>
 
 int gameBoard[9][9];
 char playerName[256];
 
+/*
+ * print sudoku field
+ */
 void printField(){
     printf("Spieler: %s\n", playerName);
 
@@ -40,6 +46,34 @@ void initGameBoard(){
     }
 }
 
+int checkSqare(point p, int v){
+    int offsetX = p.x - p.x % 3;
+    int offsetY = p.y - p.y % 3;
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if(gameBoard[i + offsetX][j+offsetY] == v){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int isValueValid(point p, int v){
+    for(int i = 0; i < 9; i++){
+        if(gameBoard[p.x][i] == v){
+            return 0;
+        }
+    }
+    for(int i = 0; i < 9; i++){
+        if(gameBoard[i][p.y] == v){
+            return 0;
+        }
+    }
+    return checkSqare(p, v);
+}
+
 /*
  * save user inputted coordinate/value
  */
@@ -47,8 +81,71 @@ void saveValue(int x, int y, int value){
     gameBoard[x][y] = value;
 }
 
+int removeValue(int x, int y){
+    int number = gameBoard[x][y];
+    gameBoard[x][y] = 0;
+    return number;
+}
+
+point getNewRandomPoint(){
+    point p;
+    do{
+        int randomNumber = (rand() % 9);
+        p.x = randomNumber;
+        randomNumber = (rand() % 9);
+        p.y = randomNumber;
+    } while(0 != gameBoard[p.x][p.y]);
+
+    return p;
+}
+
+/*
+ * fill game board with random (correct) values
+ */
+void fillField(){
+    myStack_t* stack;
+    stack = StackNew(sizeof(point), 81 );
+    int counter = 0; // currently found numbers
+    int back = 0; //backtracking indicator
+    int number = 1; // number to check
+    point p;
+
+    while(counter < 81){
+        if(!back){
+            p = getNewRandomPoint();
+            number = 1;
+        }
+
+        int isValidNumber = 0;
+        while (number < 10){
+            if(isValueValid(p, number)){
+                isValidNumber = 1;
+                break;
+            }
+            number++;
+        }
+
+        if(isValidNumber){
+            back = 0;
+            saveValue(p.x, p.y, number);
+            Push(stack, &p);
+            counter++;
+        } else {
+            Pop(stack, &p);
+            number = removeValue(p.x, p.y)+1;
+            back = 1;
+            counter--;
+        }
+    }
+}
+
+/*
+ *
+ */
 int main(int argc, char** argv) {
+    srand(time(0));
     initGameBoard();
+    fillField();
     printf("Bitte Namen eingeben: ");
     scanf("%255[^\n]", playerName);
     printField();
---
+++ point.h
@@ -0,0 +1,13 @@
+//
+// Created by felix on 27.01.2022.
+//
+
+#ifndef SUDOKU_POINT_H
+#define SUDOKU_POINT_H
+
+typedef struct{
+    int x;
+    int y;
+} point;
+
+#endif //SUDOKU_POINT_H
